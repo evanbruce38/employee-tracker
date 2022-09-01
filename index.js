849b9c001034204b947bfb4cb329bb7a789ecf6e
evanbruce38 <106777653+evanbruce38@users.noreply.github.com>
2022-09-01 17:05:16 -0400

    working on inquirer main prompt

--- index.js
+++ index.js
@@ -10,7 +10,42 @@ const connection = mysql2.createConnection({
 });
 
 connection.connect(() => {
+    console.log('database connected');
+});
+
+const mainPrompt = () => {
     inquirer.prompt({
-    
-    })
+    type: 'list',
+    name: 'action',
+    message: 'What would you like to do?',
+    choices: ['view all departments', 'view all roles', 'add a department', 'add a role', 'add an employee', 'update an employee role', 'exit the application']
 })
+.then((answers) => {
+    switch(answers.action){
+        case 'view all departments':
+            viewDepartments();
+            break;
+        case 'view all roles':
+            viewRoles();
+            break;
+        case 'view all employees':
+            viewEmployees();
+            break;
+        case 'add a department':
+            addDepartment();
+            break;
+        case 'add a role':
+            addRole();
+            break;
+        case 'add an employee':
+            addEmployee();
+            break;
+        case 'update an employee role':
+            updateRole();
+            break;
+        case 'exit the application':
+            console.log('See you later!')
+            process.exit(0);
+        };
+    });
+};
